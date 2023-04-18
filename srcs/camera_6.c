df08133171319781c2b48d2aca4aa58727b40ac4
Nuttapon Nakarach <nnakarac@z3t4c3.42bangkok.com>
2023-04-18 19:57:20 +0700

    camera6

--- srcs/camera_6.c
+++ srcs/camera_6.c
@@ -6,14 +6,21 @@
 /*   By: nnakarac <nnakarac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/18 18:16:09 by nnakarac          #+#    #+#             */
-/*   Updated: 2023/04/18 18:20:42 by nnakarac         ###   ########.fr       */
+/*   Updated: 2023/04/18 19:57:20 by nnakarac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "camera.h"
 #include "v_util.h"
 
+/// @brief Function to compute camera geometry
+/// @note 1. Compute a vector from the camera position to the LookAt position.
+/// @note 2. Compute the U and V vectors.
+/// @param camera
 void	update_cam_geomet(t_camera *camera)
 {
-	camera->v_align_vect =
+	camera->v_align_vect = nml_mat_sub(camera->v_cam_lookat, \
+		camera->v_cam_pos);
+	nml_mat_normalize_r(camera->v_align_vect);
+	camera->v_proj_scr_u =
 }

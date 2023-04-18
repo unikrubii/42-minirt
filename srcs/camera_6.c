9a14534dcf995e22d12786f4b0521258dc48ca05
Nuttapon Nakarach <nnakarac@z3t4c3.42bangkok.com>
2023-04-18 19:58:00 +0700

    need to add cross vector

--- srcs/camera_6.c
+++ srcs/camera_6.c
@@ -6,7 +6,7 @@
 /*   By: nnakarac <nnakarac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/18 18:16:09 by nnakarac          #+#    #+#             */
-/*   Updated: 2023/04/18 19:57:20 by nnakarac         ###   ########.fr       */
+/*   Updated: 2023/04/18 19:57:32 by nnakarac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,5 @@ void	update_cam_geomet(t_camera *camera)
 	camera->v_align_vect = nml_mat_sub(camera->v_cam_lookat, \
 		camera->v_cam_pos);
 	nml_mat_normalize_r(camera->v_align_vect);
-	camera->v_proj_scr_u =
+	// camera->v_proj_scr_u =
 }

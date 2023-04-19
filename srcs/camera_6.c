c5df732a23e2a28ed32d4605f4d5dcf98c6e4e3f
Nuttapon Nakarach <nnakarac@Nuttapons-MacBook-Air.local>
2023-04-19 22:47:07 +0700

    cam6 not completed

--- srcs/camera_6.c
+++ srcs/camera_6.c
@@ -6,7 +6,7 @@
 /*   By: nnakarac <nnakarac@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/18 18:16:09 by nnakarac          #+#    #+#             */
-/*   Updated: 2023/04/19 22:28:04 by nnakarac         ###   ########.fr       */
+/*   Updated: 2023/04/19 22:46:56 by nnakarac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,24 @@
 /// @brief Function to compute camera geometry
 /// @note 1. Compute a vector from the camera position to the LookAt position.
 /// @note 2. Compute the U and V vectors.
+/// @note 3. Compute the position of the center point of the screen.
+/// @note 4. Modify U and V vectors to match the size and aspect ratio.
 /// @param camera
 void	update_cam_geomet(t_camera *camera)
 {
+	t_nml_mat	*smult_tmp;
+
 	camera->v_align_vect = nml_mat_sub(camera->v_cam_lookat, \
 		camera->v_cam_pos);
 	nml_mat_normalize_r(camera->v_align_vect);
 	camera->v_proj_scr_u = vect_cross(camera->v_align_vect, camera->v_cam_up);
 	nml_mat_normalize_r(camera->v_proj_scr_u);
+	camera->v_proj_scr_v = vect_cross(camera->v_proj_scr_u, \
+		camera->v_align_vect);
+	nml_mat_normalize_r(camera->v_proj_scr_v);
+	smult_tmp = nml_mat_smult(camera->v_align_vect, camera->cam_len);
+	camera->v_proj_scr_cen = nml_mat_add_r(camera->v_cam_pos, smult_tmp);
+	nml_mat_free(smult_tmp);
+	nml_mat_smult_r(camera->v_proj_scr_u, camera->cam_hor_size);
 
 }

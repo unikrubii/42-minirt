49e6c67667a19fccffaf36e504edd176ff879042
Sirilert Thitikul <rubii@Rubii-Mac.local>
2023-05-01 21:01:03 +0700

    freeze branch for now

--- includes/readmap.h
+++ includes/readmap.h
@@ -4,7 +4,10 @@
 # include "libft.h"
 # include "get_next_line.h"
 
-typedef struct object_scene
+typedef struct s_object
+{
+	
+}
 
 int		get_row(char *path);
 char	**get_map(char *path, int row_count);

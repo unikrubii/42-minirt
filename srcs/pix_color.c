cbce9e2f836585d3ba11837c8e84b20b676cd658
Nuttapon Nakarach <nnakarac@Nuttapons-MacBook-Air.local>
2023-06-05 14:51:44 +0700

    add pix color

---
+++ srcs/pix_color.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pix_color.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nnakarac <nnakarac@42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/03 12:43:42 by nnakarac          #+#    #+#             */
+/*   Updated: 2023/06/05 14:46:47 by nnakarac         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minirt.h"
+
+void	init_color_pix(t_handle *handy)
+{
+	int			width;
+	int			height;
+	t_nml_mat	**dst;
+
+	// dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+
+	width = 0;
+	height = 0;
+	// init all pixel color
+	handy->vv_color = malloc(sizeof(t_nml_mat *) * WIDTH * HEIGHT);
+	while (width < WIDTH)
+	{
+		height = 0;
+		while (height < HEIGHT)
+		{
+			dst = handy->vv_color + (HEIGHT * width) + (height);
+			*dst = new_vector();
+			height++;
+		}
+		width++;
+	}
+}
+
+void	deinit_color_pix(t_handle *handy)
+{
+	int			width;
+	int			height;
+	t_nml_mat	**dst;
+
+	// dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+
+	width = 0;
+	height = 0;
+	while (width < WIDTH)
+	{
+		height = 0;
+		while (height < HEIGHT)
+		{
+			dst = handy->vv_color + (HEIGHT * width) + (height);
+			nml_mat_free(*dst);
+			height++;
+		}
+		width++;
+	}
+	free(handy->vv_color);
+}
